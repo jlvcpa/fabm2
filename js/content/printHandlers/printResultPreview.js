f15fd6dcb06f17ff191b826b0ccd63cb930c196c
jlvcpa <dpi.acct@gmail.com>
2026-03-22 12:10:06 +0800

    Update printResultPreview.js

--- js/content/printHandlers/printResultPreview.js
+++ js/content/printHandlers/printResultPreview.js
@@ -31,7 +31,7 @@ export const handlePrint = (mode, setPrintMode) => {
         @media print {
             @page {
                 size: auto;
-                margin: 0.35in;
+                margin: 0.5in;
             }
 
             html, body {
