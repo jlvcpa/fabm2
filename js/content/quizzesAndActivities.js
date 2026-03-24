a2e0204220f8d11291ed914d29ee813664a47a89
jlvcpa <dpi.acct@gmail.com>
2026-03-25 06:19:29 +0800

    Update quizzesAndActivities.js

--- js/content/quizzesAndActivities.js
+++ js/content/quizzesAndActivities.js
@@ -1,4 +1,3 @@
-// ---
 import { getFirestore, collection, getDocs, doc, getDoc, setDoc, query, where, orderBy } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
 import { getLetterGrade } from "../utils.js"; 
